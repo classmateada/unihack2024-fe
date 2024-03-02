b4df298e18713da920de361c6fe646d61c909f68
Tenny235 <100686998+Tenny235@users.noreply.github.com>
2024-03-02 14:29:08 +1100

    add greetings

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,37 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 from flask import Flask
 
 app = Flask(__name__)
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def cutoffRole(message):
+    prefix = "Interviewer: "
+    if message.startswith(prefix):
+        return message[len(prefix):]
+    else:
+        return message
+
+@app.route('/greeting')
+def greeting():
+    greetings_completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an interviewer asking behavioural questions, perform a mock interview with the user. Greet and welcome the user first. Do not start asking behavioural questions. (respond in one sentence)"}
+        ]
+    )
+
+    greetings = cutoffRole(greetings_completion.choices[0].message.content)
+    
+    return greetings
+
+
+
+
+
 
 
 if __name__ == '__main__':
