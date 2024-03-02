c3d7f23cdfa6aa1f6adc00b4824ac0d382db4497
ASEANITdirector <59725343+HardcoreTrash@users.noreply.github.com>
2024-03-02 16:16:00 +1100

    Added basic metric system

---
+++ frontend/src/components/FeedbackSystem.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+interface AnimatedProgressBarProps {
+    name: string
+    value: number;
+    duration: number;
+}
+
+const AnimatedProgressBar: React.FC<AnimatedProgressBarProps> = ({ name, value, duration, feedback }) => {
+  const [progress, setProgress] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    let start = 0;
+    const end = value;
+
+    const animationDuration = duration * 100;
+    const incrementTime = (animationDuration / end) * 200;
+
+    const timer = setInterval(() => {
+      start += 1;
+      setProgress(start);
+      if (start === end) clearInterval(timer);
+    }, incrementTime);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [value, duration]);
+
+    const handleToggleTextBox = () => {
+        setIsOpen(!isOpen);
+    };
+
+  return (
+    <div className = "flex flex-col justify-center items-center cursor-pointer ${isOpen ? 'cursor-default' : ''}`"
+        style={{ width: '300px'}}
+        onClick={handleToggleTextBox}
+        >
+        <h1 className="text-3xl font-bold text-blue-500 text-center"> {name} </h1>
+      <div
+        style={{ width: '150px'}}
+        ><CircularProgressbar
+        value={progress}
+        text={`${progress}%`}
+        styles={buildStyles({
+          pathTransitionDuration: 1,
+          pathColor: `rgba(62, 152, 199, ${progress / 100})`,
+          textColor: '#f88',
+          trailColor: '#d6d6d6',
+          backgroundColor: '#3e98c7',
+        })}
+      />
+    </div>
+      {isOpen && (
+        <div className="mt-2 p-2 border border-gray-300 rounded text-center flex items-center" style={{ width: '300px', height: '200px' }}>
+          <p> {feedback}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AnimatedProgressBar;
